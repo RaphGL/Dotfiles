144ca1053ffeb0b161e4e983d443960e2224e690
Rafael Lopes <rafalopes@live.com>
2020-01-10 15:27:51 -0100

    minor changes

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,6 +1,11 @@
 #!/bin/zsh
+# Visual candy
 nitrogen --restore &
 picom &
+# Keyboard layout 
 ibus-daemon -d
 ibus restart
+# Screenshot program
+flameshot
+# Switch Caps Lock and Escape keys
 setxkbmap -option caps:swapescape
