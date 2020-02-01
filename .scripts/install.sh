6e627683cdf740f2edb9a44a5c3598f2763f9a48
Rafael Lopes <rafalopes@live.com>
2020-02-01 22:34:37 -0100

    Improving script

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 cd $HOME
+RED='\033[0;31m'
+BLUE='\034[0;31m'
+NC='\033[0m' # No Color
 mkdir -p ~/.zsh-plugins
 # Installing Vim-Plug
+echo -e "${BLUE}Installing Vim-Plug...${NC}"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #Installing "Oh-My-Zsh!" and Syntax Highlighting
+echo -e "${BLUE}Setting up Zsh...${NC}"
 git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.zsh-plugins/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-plugins/zsh-syntax-highlighting
 
 # Installing dotfiles
+echo -e "\033[0;34m Setting up dotfiles...${NC}"
 rm -rf .bashrc .vimrc .Xresources .zshenv .zshrc .config/rofi/config.rasi .config/mimeapps.list 
 cd $HOME/dotfiles
 mkdir -p .temp/
@@ -18,4 +24,5 @@ stow --adopt *
 mv .temp/* $HOME/dotfiles/
 rmdir .temp/
 
+echo -e "${RED}Warning: Check your .Xresources and change it to appropriate fonts${NC}"
 chsh -s $(which zsh)
--- .scripts/pacin.sh
+++ .scripts/pacin.sh
@@ -3,3 +3,5 @@ pacman --needed -S gnome-font-viewer lxappearance code telegram-desktop thunar n
 	zsh flameshot qtile yay nitrogen surf stow rxvt-unicode zathura zathura-pdf-mupdf curl git calcurse anki imagemagick feh \
 	xorg-xrandr xorg-xdpyinfo fcitx fcitx-im fcitx-mozc sxiv ranger pass xclip gscan2pdf youtube-dl picom \
 	adobe-source-han-sans-jp-fonts adobe-source-code-pro-fonts obs-studio thunderbird
+
+pip install pynvim
