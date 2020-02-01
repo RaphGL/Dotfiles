3c6e75881816feeedc8f6779d4bab3331dee62ef
Rafael Lopes <rafalopes@live.com>
2020-02-01 19:29:19 -0100

    Testing dotfiles

--- .scripts/insdotf.sh
+++ .scripts/insdotf.sh
@@ -1,11 +1,4 @@
 #!/bin/env bash
-cd $HOME
-# Downloading Packages
-pacman --needed -S gnome-font-viewer lxappearance code telegram-desktop thunar neofetch neovim npm python-pip rofi \
-	zsh flameshot qtile yay nitrogen surf stow rxvt-unicode zathura zathura-pdf-mupdf curl git calcurse anki imagemagick feh \
-	xorg-xrandr xorg-xdpyinfo fcitx fcitx-im fcitx-mozc sxiv ranger pass xclip gscan2pdf youtube-dl picom \
-	adobe-source-han-sans-jp-fonts adobe-source-code-pro-fonts obs-studio thunderbird
-
 # Setting up Vim-plug for Neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
