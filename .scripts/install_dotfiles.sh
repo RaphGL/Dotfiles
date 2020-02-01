75754f25aec0bca54c9a9d1f7ce519b8b49c9d80
Raph <rafalopes@live.com>
2020-02-01 19:11:47 -0100

    Minor fix

--- .scripts/install_dotfiles.sh
+++ .scripts/install_dotfiles.sh
@@ -4,7 +4,7 @@ cd $HOME
 pacman --needed -S gnome-font-viewer lxappearance code telegram-desktop thunar neofetch neovim npm python-pip rofi \
 	zsh flameshot qtile yay nitrogen surf stow rxvt-unicode zathura zathura-pdf-mupdf curl git calcurse anki imagemagick feh \
 	xorg-xrandr xorg-xdpyinfo fcitx fcitx-im fcitx-mozc sxiv ranger pass xclip gscan2pdf youtube-dl picom \
-	adobe-source-han-sans-jp-fonts adobe-source-code-pro-fonts obs thunderbird
+	adobe-source-han-sans-jp-fonts adobe-source-code-pro-fonts obs-studio thunderbird
 
 # Setting up Vim-plug for Neovim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
