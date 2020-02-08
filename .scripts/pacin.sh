5538a0692e01e6fe85fde8a9ea70d6bbedb8d84c
Rafael Lopes <rafalopes@live.com>
2020-02-07 23:19:27 -0100

    Minor changes

--- .scripts/pacin.sh
+++ .scripts/pacin.sh
@@ -2,7 +2,7 @@
 pacman --needed -S gnome-font-viewer lxappearance code telegram-desktop thunar neofetch neovim npm python-pip rofi \
 	zsh flameshot qtile yay nitrogen surf stow rxvt-unicode zathura zathura-pdf-mupdf curl git calcurse anki imagemagick feh \
 	xorg-xrandr xorg-xdpyinfo fcitx fcitx-im fcitx-mozc sxiv ranger pass xclip gscan2pdf youtube-dl picom kdenlive \
-	adobe-source-han-sans-jp-fonts adobe-source-code-pro-fonts obs-studio thunderbird cmus cmake gimp w3m
+	adobe-source-han-sans-jp-fonts adobe-source-code-pro-fonts obs-studio thunderbird cmus cmake gimp w3m libnotify dunst
 
 pip install pynvim
 npm install -g neovim
