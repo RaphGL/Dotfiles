d9d311643f7e27bed6e4495929c53308d98c34ad
Rafael Lopes <rafalopes@live.com>
2020-02-20 13:15:07 -0100

    Fixed notification on launch and closure

--- .scripts/cmus_daemon.sh
+++ .scripts/cmus_daemon.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+get_status()
+{
+ cmus-remote -Q | sed -n -r "s/file\s//; s/\/home\/\w+\/Music\///; s/\.\w*$//;2p"
+}
+
 while :
 do
 	# Checks if cmus is running
@@ -6,15 +11,20 @@ do
 	if [ $? -eq 0 ]
 	then
 		# Checks whether its the same music playing
-		PLAYING=<(cmus-remote -Q | sed -n -r "s/file\s//; s/\/home\/\w+\/Music\///; s/\.\w*$//;2p")
+		PLAYING=<(get_status)
 		sleep 1
-		PLAYING_2=<(cmus-remote -Q | sed -n -r "s/file\s//; s/\/home\/\w+\/Music\///; s/\.\w*$//;2p")
+		PLAYING_2=<(get_status)
 		diff -q $PLAYING $PLAYING_2
 
 		# Sends a notification if it's a different song
 		if [ $? -eq 1 ]
 		then
-			notify-send "Now playing" "$(cmus-remote -Q | sed -n -r "s/file\s//; s/\/home\/\w+\/Music\///; s/\.\w*$//;2p")" -t 5000
+			SONG=$(get_status)
+			echo $SONG | grep "set aaa_mode artist"
+			if [ $? -ne 0 ] && [ "$SONG" != "" ]
+			then
+				notify-send "Now playing" "$SONG" -t 5000
+			fi
 		fi
 	fi
 done
