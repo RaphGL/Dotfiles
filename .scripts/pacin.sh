67065e69822d05c6c803029b2512c3bf69268ac8
Rafael Lopes <rafalopes@live.com>
2020-03-05 14:05:06 -0100

    Minor fixes

--- .scripts/pacin.sh
+++ .scripts/pacin.sh
@@ -3,8 +3,8 @@ pacman --needed -S \
 	adobe-source-han-sans-jp-fonts adobe-source-code-pro-fonts obs-studio thunderbird cmus cmake gimp w3m libnotify dunst \
 	gnome-font-viewer lxappearance code telegram-desktop nemo neofetch neovim npm python-pip rofi feh sxhkd mpv tldr hub \
 	xorg-xrandr xorg-xdpyinfo fcitx fcitx-im fcitx-mozc sxiv ranger pass xclip gscan2pdf youtube-dl picom kdenlive libreoffice-still \
-	zsh flameshot qtile surf xdotool stow kitty zathura zathura-pdf-mupdf curl git calcurse anki imagemagick pulseaudio pulseaudio-alsa
-	alsa-utils pamixer \
+	zsh flameshot qtile surf xdotool stow kitty zathura zathura-pdf-mupdf curl git calcurse anki imagemagick pulseaudio pulseaudio-alsa \
+	alsa-utils pamixer firefox htop base-devel \
 
 pip install pynvim
 npm install -g neovim
