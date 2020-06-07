6d3fdc71c266dab028601de590c575914e788524
Rafael Lopes <rafalopes@live.com>
2020-06-07 12:33:34 -0100

    Added more packages to .list

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # Set wallpaper
-feh --bg-fill ~/dotfiles/wallpapers/Pictures/Wallpapers/wallpaper5.jpg
+feh --bg-fill ~/Pictures/Wallpapers/wallpaper5.jpg
 # Lock screen on inactivity
 xautolock -time 10 -locker "i3lock -i ~/Pictures/Wallpapers/lockscreen.png" -killer "systemctl suspend" -killtime 10 &
 # Compositor
