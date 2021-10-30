4a2fb5c8e830a5503c89db979582821b209b1bc4
RaphGL <rafalopes@live.com>
2021-10-30 12:18:03 +0100

    minor changes to qtile

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -15,32 +15,34 @@ color = {
     'floating': '#8be9fd',
 }
 
+
 @hook.subscribe.startup_once
 def autostart():
     autostart = os.path.expanduser('~/.config/qtile/autostart.sh')
     os.system(f'{autostart}')
 
+
 def set_keybindings():
     return [
-        #General Key bindings
+        # General Key bindings
         Key([mod], "w", lazy.window.kill()),
         Key([mod, "control"], "0", lazy.shutdown()),
         Key([mod, "control"], "r", lazy.restart()),
         Key([mod], "Tab", lazy.next_layout()),
 
-        #Move between windows
+        # Move between windows
         Key([mod], "j", lazy.layout.down()),
         Key([mod], "k", lazy.layout.up()),
         Key([mod], "h", lazy.layout.left()),
         Key([mod], "l", lazy.layout.right()),
 
-        #Change window size
+        # Change window size
         Key([mod], "i", lazy.layout.grow()),
         Key([mod], "u", lazy.layout.shrink()),
         Key([mod], "y", lazy.layout.normalize()),
         Key([mod], "o", lazy.layout.maximize()),
 
-        #Move the windows around
+        # Move the windows around
         Key([mod, "control"], "j", lazy.layout.shuffle_down()),
         Key([mod, "control"], "k", lazy.layout.shuffle_up()),
         Key([mod, "control"], "h", lazy.layout.swap_left()),
@@ -50,6 +52,7 @@ def set_keybindings():
         Key([mod, "control"], "space", lazy.window.toggle_floating()),
         Key([mod], "f", lazy.window.toggle_fullscreen()),
     ]
+
 
 def set_groups(g: str):
     groups = [Group(i) for i in g]
@@ -64,12 +67,16 @@ def set_groups(g: str):
         )
     return groups
 
-#My preferred layouts
+# My preferred layouts
+
+
 def set_layouts():
     return [
-         layout.MonadTall(border_focus=color['active'], border_width=2, margin=8),
+        layout.MonadTall(
+            border_focus=color['active'], border_width=2, margin=8),
         layout.Max(),
     ]
+
 
 def set_widget_defaults():
     return {
@@ -78,42 +85,43 @@ def set_widget_defaults():
         'padding': 4,
     }
 
+
 def set_widgets():
     def arrow_widget(theme):
         if 'end' in theme:
-            image = f'~/.config/qtile/{theme}.png' 
+            image = f'~/.config/qtile/{theme}.png'
         else:
-            image = f'~/.config/qtile/powerline{theme}.png' 
+            image = f'~/.config/qtile/powerline{theme}.png'
         return widget.Image(filename=image)
     return [
-       #Groups and Window names
-       widget.GroupBox(inactive=color['inactive'], disable_drag=True, hide_unused=True, this_current_screen_border=color['active'], urgent_border=color['urgent'], highlight_method='block', rounded=False, use_mouse_wheel=False, padding_x=1),
-       widget.WindowName(),
-       confirm_prompt,
-       # Battery widget
-       arrow_widget('endlight'),
-       widget.Image(filename='~/.config/qtile/icons/batterylight.png'),
-        widget.Battery(format='{char} {percent:2.0%} {hour:d}:{min:02d}', background=color['active']),
-       # Volume control
-       arrow_widget('dark'),
-       widget.Image(filename='~/.config/qtile/icons/volumedark.png'),
-       widget.Volume(volume_app="pulseaudio", background=color['inactive']),
-       # System Resources 
-       arrow_widget('light'),
-       widget.Image(filename='~/.config/qtile/icons/memorylight.png'),
-       widget.Memory(background=color['active']),
-       arrow_widget('dark'),
-       widget.Image(filename='~/.config/qtile/icons/cpudark.png'),
-       widget.CPUGraph(graph_color=color['floating'], border_color=color['active'], background=color['inactive'], type='box'),
-       # Date and time widget
-       arrow_widget('light'),
-       widget.Image(filename='~/.config/qtile/icons/calendarlight.png'),
-        widget.Clock(background=color['active'], foreground=color['foreground'], format='%A %Y/%m/%d %H:%M'),
-       # System tray
-       arrow_widget('dark'),
-       widget.Image(filename='~/.config/qtile/icons/systraydark.png'),
-       widget.Systray(icon_size=21, padding=6, background=color['inactive']),
+        # Groups and Window names
+        widget.GroupBox(inactive=color['inactive'], disable_drag=True, hide_unused=True, this_current_screen_border=color['active'],
+                        urgent_border=color['urgent'], highlight_method='block', rounded=False, use_mouse_wheel=False, padding_x=1),
+        widget.WindowName(format='{name}'),
+        confirm_prompt,
+        # Battery widget
+        arrow_widget('enddark'),
+        widget.Image(filename='~/.config/qtile/icons/batterydark.png'),
+        widget.Battery(format='{char} {percent:2.0%}',
+                       background=color['inactive']),
+        # Volume control
+        arrow_widget('light'),
+        widget.Image(filename='~/.config/qtile/icons/volumelight.png'),
+        widget.Volume(volume_app="pulseaudio", background=color['active']),
+        # System Resources
+        arrow_widget('dark'),
+        widget.Image(filename='~/.config/qtile/icons/memorydark.png'),
+        widget.Memory(background=color['inactive']),
+        # Date and time widget
+        arrow_widget('light'),
+        widget.Image(filename='~/.config/qtile/icons/calendarlight.png'),
+        widget.Clock(
+            background=color['active'], foreground=color['foreground'], format='%A %Y/%m/%d %H:%M'),
+        # System tray
+        arrow_widget('dark'),
+        widget.Systray(icon_size=21, padding=6, background=color['inactive']),
     ]
+
 
 keys = set_keybindings()
 layouts = set_layouts()
@@ -123,7 +131,7 @@ screens = [
     Screen(
         top=bar.Bar(
             set_widgets(),
-            25, background=color['background'],
+            30, background=color['background'],
         ),
     ),
 ]
@@ -131,9 +139,9 @@ screens = [
 # Drag floating layouts.
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(),
-        start=lazy.window.get_position()),
+         start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(),
-        start=lazy.window.get_size()),
+         start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
@@ -144,7 +152,8 @@ main = None
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
-floating_layout = layout.Floating(border_focus=color['floating'], border_width=2)
+floating_layout = layout.Floating(
+    border_focus=color['floating'], border_width=2)
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 extentions = []
