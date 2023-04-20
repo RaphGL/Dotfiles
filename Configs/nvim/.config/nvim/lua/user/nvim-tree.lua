1e182334e6f1a95ce21db936072da66a88c17b69
RaphGL <raphfl.dev@gmail.com>
2023-04-20 10:30:37 -0100

    updated nvim-tree

--- Configs/nvim/.config/nvim/lua/user/nvim-tree.lua
+++ Configs/nvim/.config/nvim/lua/user/nvim-tree.lua
@@ -11,6 +11,20 @@ end
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
+  on_attach = function(bufnr)
+      local api = require('nvim-tree.api')
+
+      local function opts(desc)
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
+      vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+      vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
+      vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
+      vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+      vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+  end,
+
   update_focused_file = {
     enable = true,
     update_cwd = true,
@@ -56,12 +70,5 @@ nvim_tree.setup {
   view = {
     width = 30,
     side = "left",
-    mappings = {
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
   },
 }
