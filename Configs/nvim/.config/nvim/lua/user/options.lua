8159bac793cf63202ebe73fcc6b1ac6e90a1bb45
RaphGL <raphfl.dev@gmail.com>
2023-04-22 17:24:43 -0100

    made line numbers relative

--- Configs/nvim/.config/nvim/lua/user/options.lua
+++ Configs/nvim/.config/nvim/lua/user/options.lua
@@ -2,6 +2,7 @@ vim.opt.backup = false                          -- creates a backup file
 vim.opt.cmdheight = 1                           -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
+vim.opt.relativenumber = true                   -- make line numbers relative to the cursor
 vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
 vim.opt.hlsearch = true                         -- highlight all matches on previous search pattern
 vim.opt.ignorecase = true                       -- ignore case in search patterns
