82c5d0161a67379f83cab94471be679c4248b10d
RaphGL <raphfl.dev@gmail.com>
2023-06-18 15:24:59 -0100

    fixed nvim-tree and removed packages from list

--- Configs/nvim/.config/nvim/lua/user/nvim-tree.lua
+++ Configs/nvim/.config/nvim/lua/user/nvim-tree.lua
@@ -12,17 +12,18 @@ local tree_cb = nvim_tree_config.nvim_tree_callback
 
 nvim_tree.setup {
   on_attach = function(bufnr)
-      local api = require('nvim-tree.api')
+    local api = require "nvim-tree.api"
 
-      local function opts(desc)
-        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-      end
+    local function opts(desc)
+      return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
 
-      vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
-      vim.keymap.set('n', '<CR>', api.node.open.edit, opts('Open'))
-      vim.keymap.set('n', 'o', api.node.open.edit, opts('Open'))
-      vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
-      vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- custom mappings
+    vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent,        opts('Up'))
+    vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
   end,
 
   update_focused_file = {
